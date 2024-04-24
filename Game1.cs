accc966b4fc6bdcbecd2cb9cc256d693cf77162f
Logan Boughner <339597882@GoTVDSB.ca>
2024-04-24 13:12:30 -0400

    Done

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,11 @@ namespace Mono_Topic_3._5_Mouse_Clicks
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D quitTexture;
+        Rectangle quitRect;
+        MouseState mouseState;
+        MouseState prevMouseState;
+
 
         public Game1()
         {
@@ -19,6 +24,7 @@ namespace Mono_Topic_3._5_Mouse_Clicks
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            quitRect = new Rectangle(10, 10, 200, 80);
 
             base.Initialize();
         }
@@ -28,15 +34,26 @@ namespace Mono_Topic_3._5_Mouse_Clicks
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            quitTexture = Content.Load<Texture2D>("QuitButton");
         }
 
         protected override void Update(GameTime gameTime)
         {
+            
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            prevMouseState = mouseState;
+            mouseState = Mouse.GetState();
+            
+            if (mouseState.LeftButton == ButtonState.Pressed &&
+prevMouseState.LeftButton == ButtonState.Released)
+            {
+                if (quitRect.Contains(mouseState.Position))
+                    Exit();
 
+            }
             base.Update(gameTime);
         }
 
@@ -45,6 +62,9 @@ namespace Mono_Topic_3._5_Mouse_Clicks
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(quitTexture, quitRect, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
